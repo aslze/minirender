a675f7e0f8013332830bdf69db50f2c80a275e11
Alvaro <alvaro.segura@gmail.com>
2022-11-23 23:12:35 +0100

    updated bench.cpp

--- samples/bench.cpp
+++ samples/bench.cpp
@@ -11,7 +11,7 @@ float rf(float z, float s)
 	return 30 + 40 * sin(4 * z * (float)PI / s) / (4 * z * (float)PI / s);
 }
 
-TriMesh* createObject(int m, int n, bool tex = false)
+TriMesh* createObject(int m, int n, bool usetex, Random& random)
 {
 	TriMesh* mesh = new TriMesh();
 
@@ -29,15 +29,30 @@ TriMesh* createObject(int m, int n, bool tex = false)
 			mesh->vertices << p;
 			mesh->normals << Vec3(nor.x * cos(j * da), nor.x * sin(j * da), nor.y).normalized();
 
+			if (usetex)
+				mesh->texcoords << Vec2((float)j / n, (float)i / m);
+
 			if (j > 0 && i > 0)
 				mesh->indices << (n * (i - 1) + j - 1) << (n * (i - 1) + j) << (n * i + j) << (n * (i - 1) + j - 1)
 				              << (n * i + j) << (n * i + j - 1);
 		}
 	}
+	
 	mesh->normalsI = mesh->indices;
+	if (usetex)
+		mesh->texcoordsI = mesh->indices;
 
 	mesh->material = new Material();
 	mesh->material->shininess = 15;
+
+	if (usetex) {
+		Vec3 color = { random(1.f), random(1.f), random(1.f) };
+		Array2<Vec3> tex(256, 256);
+		for (int i = 0; i < tex.rows(); i++)
+			for (int j = 0; j < tex.cols(); j++)
+				tex(i, j) = color * (0.75f + 0.25f * (cos(i * 40 / 256.f) * sin(j * 40 / 256.f)));
+		mesh->material->texture = tex;
+	}
 
 	return mesh;
 }
@@ -47,7 +62,7 @@ int main(int argc, char** argv)
 {
 	CmdArgs args;
 
-	float d = args["d"] | 680;                  // camera distance
+	float d = args["d"] | 700;                  // camera distance
 	int n = args["n"] | 1;                      // number of frames
 	int sizew = args["w"] | 1920;               // image size (not for console)
 	int sizeh = args["h"] | (sizew * 9 / 16);
@@ -56,6 +71,8 @@ int main(int argc, char** argv)
 	float fov = deg2rad(35.f);
 	float yaw = deg2rad(float(args["yaw"] | 0));
 	float tilt = deg2rad(float(args["tilt"] | 20));
+	bool  usetex = args.has("tex");
+	bool  nolight = args.has("dark");
 
 	bool saving = args.has("save");
 
@@ -65,7 +82,7 @@ int main(int argc, char** argv)
 
 	for (int i = 0; i < 20; i++)
 	{
-		auto shape = createObject(200, 200);
+		auto shape = createObject(200, 200, usetex, random);
 
 		shape->material->diffuse = { random(1.f), random(1.f) ,random(1.f) };
 		shape->transform = Matrix4::translate(random(-180.f, 180.f), random(-180.f, 180.f), random(-100.f, 100.f)) * Matrix4::rotate({ random(1.f), random(1.f), random(1.f) });
@@ -81,11 +98,11 @@ int main(int argc, char** argv)
 	Renderer renderer;
 
 	renderer.setLight(Vec3(-0.4f, .6f, 1.f));
-	//renderer.setLight({ 0, 0, 0 }, true);
+	//enderer.setLight({ 0, 0, 0 }, true);
 	renderer.setScene(scene);
 	renderer.setSize(sizew, sizeh);
 	renderer.setProjection(projectionFrustum(fov, renderer.aspect(), 10, 7000));
-	//renderer.setLighting(false);
+	renderer.setLighting(!nolight);
 
 	Array<double> times;
 
